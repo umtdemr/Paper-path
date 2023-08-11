72a7162a48feb11ce6d266eebe7c2f195712b83f
umtdemr <umitde692@gmail.com>
2023-08-11 22:29:05 +0300

    finish lasso deleting implementation

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,38 +1,55 @@
 import { useRef, useEffect } from 'react';
 import Paper, { Point, Path } from 'paper';
 
+interface IPaperMouseEvent extends MouseEvent {
+    point: paper.Point ;
+}
+
+let drawingCount = 0;
+
 const draw1 = () => {
     const myPath = new Path();
-    const myPath2 = new Path();
-    myPath.strokeColor = 'red';
-    myPath2.strokeColor = 'blue';
-    myPath.add(new Point(300, 50));
-    myPath.add(new Point(300, 100));
-    myPath.add(new Point(400, 100)); 
-    myPath.add(new Point(400, 50)); 
-    myPath.add(new Point(300, 50)); 
+    const lassoPath = new Path();
+    myPath.strokeColor = '#000';
+    myPath.strokeWidth = 3;
+    lassoPath.strokeColor = 'red';
+    lassoPath.strokeWidth = 3;
+    lassoPath.dashArray = [5, 5];
 
-    myPath2.add(new Point(360, 50));
-    myPath2.add(new Point(360, 100));
-    myPath2.add(new Point(500, 100)); 
-    myPath2.add(new Point(500, 50)); 
-    myPath2.add(new Point(360, 50));  
-    // const intersections = myPath.getIntersections(myPath2);
-    // console.log(intersections)
-    // for (let i = 0; i < intersections.length; i++) {
-    //     console.log(intersections[i])
-    //     new Path.Circle({
-    //         center: intersections[i].point,
-    //         radius: 5,
-    //         fillColor: '#009dec'
-    //     })
-    // }
+    Paper.view.onMouseDown = () => {
+        drawingCount++;
+        if (drawingCount > 2) {
+            myPath.removeSegments();
+            lassoPath.removeSegments();
+            drawingCount = 1;
+        }
+    };
+  
+    Paper.view.onMouseDrag = (event: IPaperMouseEvent) => {
+        if (drawingCount === 1) {
+            myPath.add(event.point);
+        }
+        else {
+            lassoPath.add(event.point);
+        }
+    };
+    Paper.view.onMouseUp = () => {
+        if (drawingCount === 2) {
+            const deletedPath = myPath.intersect(lassoPath, {
+                trace: false
+            });
+            deletedPath.strokeColor = 'blue';
 
-    const d =myPath.intersect(myPath2);
-    d.position.x += 200
-    // d.remove()
-    // myPath.remove();
-    // d.position.x += 500
+            const subtractedPath = myPath.subtract(lassoPath, {
+                trace: false
+            })
+            subtractedPath.strokeColor = 'green';
+
+            subtractedPath.position.x = myPath.position.x + myPath.bounds.width + 50;
+            deletedPath.position.x = subtractedPath.position.x;
+            deletedPath.position.y = subtractedPath.position.y + subtractedPath.bounds.height + 50;
+        }
+    }
 
 };
 
